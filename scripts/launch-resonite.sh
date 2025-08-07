2bfc64afd24c201c5ceccb358f1929e4a498e399
Foohy <foohyf@gmail.com>
2025-08-06 23:28:48 -0700

    Allow overriding dotnet launch entrypoint

--- scripts/launch-resonite.sh
+++ scripts/launch-resonite.sh
@@ -6,8 +6,15 @@ find /Logs -type f -name *.log -atime +${LOG_RETENTION:-30} -delete
 
 cd /home/container/Headless/
 
+RESONITE_ENTRYPOINT="Resonite.dll"
+
+if [ ! "${OVERRIDE_ENTRYPOINT}" = "" ]; then
+	RESONITE_ENTRYPOINT="${OVERRIDE_ENTRYPOINT}"
+fi
+
+
 if [ "${ENABLE_MODS}" = "true" ]; then
-	exec dotnet Resonite.dll -HeadlessConfig /Config/${CONFIG_FILE} -Logs /Logs/ -LoadAssembly Libraries/ResoniteModLoader.dll ${ADDITIONAL_ARGUMENTS}
+	exec dotnet ${RESONITE_ENTRYPOINT} -HeadlessConfig /Config/${CONFIG_FILE} -Logs /Logs/ -LoadAssembly Libraries/ResoniteModLoader.dll ${ADDITIONAL_ARGUMENTS}
 else
-	exec dotnet Resonite.dll -HeadlessConfig /Config/${CONFIG_FILE} -Logs /Logs/ ${ADDITIONAL_ARGUMENTS}
+	exec dotnet ${RESONITE_ENTRYPOINT} -HeadlessConfig /Config/${CONFIG_FILE} -Logs /Logs/ ${ADDITIONAL_ARGUMENTS}
 fi
